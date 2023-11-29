e48e7505788c913ec26232483a84bec8c6f28897
ZjzMisaka <zjzmisaka@outlook.com>
2023-11-29 10:49:31 +0800

    +ResourceHelper

--- GlyphCode/MainWindow.xaml.cs
+++ GlyphCode/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,19 @@ namespace GlyphCode
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Dictionary<string, string> strokeOrderDic = new Dictionary<string, string>();
+        private Dictionary<string, bool[,]> charactersDic = new Dictionary<string, bool[,]>();
+        private Dictionary<string, bool[,]> numbersDic = new Dictionary<string, bool[,]>();
         public MainWindow()
         {
             InitializeComponent();
+
+            ResourceHelper.ReadStrokeOrder("StrokeOrder.txt", strokeOrderDic);
+
+            ResourceHelper.ReadPng("img/Characters", charactersDic);
+            ResourceHelper.ReadPng("img/Numbers", numbersDic);
         }
+
+        
     }
 }
---
+++ GlyphCode/ResourceHelper.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GlyphCode
+{
+    internal static class ResourceHelper
+    {
+        internal static void ReadStrokeOrder(string path, Dictionary<string, string> strokeOrderDic)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        string[] parts = line.Split(new[] { ": " }, StringSplitOptions.None);
+
+                        if (parts.Length == 2)
+                        {
+                            string key = parts[0];
+                            string value = parts[1];
+
+                            if (!strokeOrderDic.ContainsKey(key))
+                            {
+                                strokeOrderDic.Add(key, value);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                return;
+            }
+        }
+
+        internal static void ReadPng(string path, Dictionary<string, bool[,]> dictionary)
+        {
+            string[] pngFiles = Directory.GetFiles(path, "*.png");
+
+            foreach (var pngFile in pngFiles)
+            {
+                Bitmap bitmap = new Bitmap(pngFile);
+                if (bitmap.Width != 16 || bitmap.Height != 16)
+                {
+                    throw new Exception($"Image {pngFile} is not 16x16 pixels.");
+                }
+
+                var transparencyData = new bool[16, 16];
+                for (int x = 0; x < 16; x++)
+                {
+                    for (int y = 0; y < 16; y++)
+                    {
+                        System.Drawing.Color pixel = bitmap.GetPixel(x, y);
+                        transparencyData[x, y] = pixel.A == 0;
+                    }
+                }
+
+                string filename = System.IO.Path.GetFileNameWithoutExtension(pngFile);
+                dictionary[filename] = transparencyData;
+            }
+        }
+    }
+}
